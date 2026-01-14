2279f2c2b9a341dbd86f2326363724d636f7aec7
Devon Jones <devon.shun.jones@gmail.com>
2026-01-14 13:16:52 -0600

    Set up database and express connection

---
+++ README.md
@@ -0,0 +1,38 @@
+# Connecting a Database 
+
+This is a server set up that uses axios to communicate with my MongoDB cluster
+
+### Description
+
+
+
+## Getting Started
+
+### Dependencies
+Express.js
+
+
+dotenv
+
+### Authors
+
+Devon Jones
+
+Github: [@Ashundere](https://github.com/Ashundere)
+
+## Version History
+
+0.1
+-Initial Release
+
+
+## Acknowledgments
+
+JS MDN: https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+W3Schools: https://www.w3schools.com/
+
+MongoDB: https://cloud.mongodb.com/
+
+
+## Reflections
---
+++ server.js
@@ -0,0 +1,27 @@
+
+
+const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const uri = process.env.MONGO_URI;
+
+const client = new MongoClient(uri);
+
+app.get('/', async (req, res) => {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    res.status(200).json({ message: "Successfully connected to the database!" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Failed to connect to the database." });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
